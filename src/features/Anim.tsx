9381d62c64de682d140115fa621f065f15705e36
user <3440771474@qq.com>
2021-12-23 19:28:31 +0800

    修复i18n的模块bug

--- src/features/Anim.tsx
+++ src/features/Anim.tsx
@@ -6,7 +6,7 @@ type LayoutProps = {
 }
 
 const Layout: React.FC<LayoutProps> = ({left, right}) => <div
-  className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 mt-20 sm:mt-24 lg:mt-48 lg:grid lg:gap-8 lg:grid-cols-12 lg:items-center">
+  className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 mt-20 sm:mt-24 lg:mt-64 lg:grid lg:gap-8 lg:grid-cols-12 lg:items-center">
   <div className="relative row-start-1 col-start-6 xl:col-start-7 col-span-7 xl:col-span-6">
     <div className="-mx-4 sm:mx-0">{right}</div>
   </div>
