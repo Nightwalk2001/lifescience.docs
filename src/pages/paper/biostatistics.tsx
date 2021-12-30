3f044bdb3786dc7435874542d67e4eae62f0676b
user <3440771474@qq.com>
2021-12-30 17:27:42 +0800

    生物统计学提交些文字先

--- src/pages/paper/biostatistics.tsx
+++ src/pages/paper/biostatistics.tsx
@@ -7,9 +7,30 @@ const biostatistics: NextPage = () => <div
   <text className={"self-start font-semibold"}>
     王志威-19331144 梁靖雯- 陈娟-
   </text>
+  <p><strong>摘要：</strong>许多患有染色体缺陷病的新生儿，如果能及早发现并辅以遗传治疗，具有不可估量的意义，如果想凭肉眼判断幼儿是否患有遗传缺陷病，往往需要丰富的经验，要让人信服又需要足够的资历，而具有丰富经验资历的医生是一个较小的群体，在理想情况下，深度学习在图像识别上可以达到99%以上的准确率，有望作为一个很好的辅助判断工具。
+  </p>
+  <p className={"self-start"}><strong>关键词：</strong>深度学习 图像识别 染色体缺陷病 儿童疾病 遗传治疗</p>
   <p className={"self-start"}>
     下面是使用各国家感染者数量画出的树图
   </p>
+  <pre className={"!w-3/4 !text-white !bg-gray-400 !px-2 !py-1.5"}>
+    {`
+    const root = hierarchy(TestData, d => d.children)
+    .sum(i => i.confirmed)
+    .sort((a, b) => b.confirmed - a.confirmed)
+    
+    const rootWithPosition = treemap().size([width, height])(root)
+
+    svg.selectAll("rect")
+      .data(rootWithPosition)
+      .join("rect")
+      .attr("x", d => d.x0)
+      .attr("y", d => d.y0)
+      .attr("width", d => d.x1 - d.x0)
+      .attr("height", d => d.y1 - d.y0)
+      .style("stroke", "#ec77c7")
+      `}
+  </pre>
   <CovidTreemap className={"w-[900px] h-[500px]"}/>
   <p className={"self-start"}>
     下面是使用各国家感染者数量画出的地理图
