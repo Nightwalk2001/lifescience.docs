0d0855659c3ab3d9d8457223cbb2ac9a508a9aa2
user <3440771474@qq.com>
2022-01-19 14:42:45 +0800

    2022

--- src/widgets/Copyright.tsx
+++ src/widgets/Copyright.tsx
@@ -3,6 +3,6 @@ import React from "react"
 export const Copyright: React.FC = () => <>
   <div className={"bg-gray-200 w-screen h-[1px] mt-10"}/>
   <div className={"w-9/12 mx-auto pt-8 pb-10 text-center"}>
-    <div>Copyright © Zhiwei Wang / kong bai / night walk.2001 —— 2021</div>
+    <div>Copyright © Zhiwei Wang / kong bai / night walk.2001 —— 2022</div>
   </div>
 </>
