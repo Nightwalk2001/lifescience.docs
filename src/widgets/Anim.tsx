2045090872c90c461d50a9ac0a53bcaca1e5227f
user <3440771474@qq.com>
2022-02-26 12:57:41 +0800

    lhy

--- src/widgets/Anim.tsx
+++ src/widgets/Anim.tsx
@@ -47,15 +47,15 @@ export const Anim: React.FC = () => <Layout
       {/*スタック、Docker、GithubなどのCI / CDツール*/}
       {/*ワークフローなど。データの視覚化に長けており、D3.jsの使用に習熟しており、手動でパスを記述できます。深層学習、注意モデル、深層強化学習、トランスフォーマー、およびその背後にある数学的原理、マトリックス理論、トポロジー、圏論などのさまざまなテクノロジーが得意です。少しのコンピュータグラフィックスを理解し、Canvasを上手に使用する*/}
       {/*API、OpenGL API、Three.js。*/}
-      掌握的图灵完备语言包括JavaScript/TypeScript、Kotlin、Swift、Java、Python、Golang、Rust、Dart、Scala、Clojure、F#、C#。<br/>
+      掌握JavaScript/TypeScript、Kotlin、Swift、Java、Python、Golang、Rust、Dart、Scala、Clojure、F#、C#等图灵完备语言。<br/>
       学过操作系统、计算机组成原理、算法与数据结构、计算机网络原理。<br/>
       擅长软件开发，包括网站、安卓、iOS、Windows、macOS，熟练使用React、Next、Remix、Gatsby、Vue、Hexo、Flutter、React
       Native、Electron、KMP、Jetpack Compose。<br/>
       擅长服务端开发，掌握几乎所有服务端框架，经典框架如Django、Express、Spring Boot熟悉其源代码，偏好Rust、Golang、Kotlin、Nodejs服务端开发。<br/>
       熟悉各类中间件如Mongodb、Redis、FLink、Kafka、Fluentd、Oauth2。<br/>
-      熟悉各类运维技术，包括Linux、K8s、Istio、Elastic Stack、Docker、CI/CD工具如Github Workflow等等。<br/>
+      熟悉各类运维技术，包括Linux、K8s、Istio、Elastic Stack、Docker、CI/CD工具如Github Workflow、Travis CI等。<br/>
       擅长数据可视化，熟练使用D3.js，可以手写path。<br/>
-      擅长深度学习，各类技术如注意力模型、深度强化学习、Transformer，及其背后的数学原理，矩阵论，拓扑学，范畴论等等。<br/>
-      明白一点计算机图形学，熟练使用Canvas API、OpenGL API及Three.js。
+      能使用深度学习、强化学习解决各类问题。<br/>
+      熟练使用Canvas API、OpenGL API及Three.js。
     </div>
   }/>
